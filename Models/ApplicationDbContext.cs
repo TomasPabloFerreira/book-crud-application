28e33352c5107f71e93c38fa6b3fcaa97687b2ac
TomasPabloFerreira <tomasferreira373@gmail.com>
2020-09-02 11:52:18 -0300

    create book table migration

---
+++ Models/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace book_crud_application.Models
+{
+	public class ApplicationDbContext : DbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+		{
+			
+		}
+
+		public DbSet<Book> Book { get; set; }
+	}
+}
