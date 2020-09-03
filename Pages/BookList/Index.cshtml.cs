bb26adac2cb5aee55134504c550fdcb2105e5625
TomasPabloFerreira <tomasferreira373@gmail.com>
2020-09-03 12:41:16 -0300

    add delete book handler

--- Pages/BookList/Index.cshtml.cs
+++ Pages/BookList/Index.cshtml.cs
@@ -24,5 +24,14 @@ namespace book_crud_application.Pages.BookList
 		{
 			Books = await _db.Book.ToListAsync();
 		}
+
+		public async Task<IActionResult> OnPostDelete(int id)
+		{
+			var book = await _db.Book.FindAsync(id);
+			if(book == null) return NotFound();
+			_db.Book.Remove(book);
+			await _db.SaveChangesAsync();
+			return RedirectToPage("Index");
+		}
 	}
 }
