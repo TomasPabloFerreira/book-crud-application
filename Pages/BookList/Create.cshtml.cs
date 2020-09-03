12e8e992b6f8713b2199a183b42a1be031661d08
TomasPabloFerreira <tomasferreira373@gmail.com>
2020-09-03 02:48:10 -0300

    add book post handler

--- Pages/BookList/Create.cshtml.cs
+++ Pages/BookList/Create.cshtml.cs
@@ -17,10 +17,22 @@ namespace book_crud_application.Pages.BookList
 			_db = db;
 		}
 
+		[BindProperty]
 		public Book Book { get; set; }
 
 		public void OnGet()
 		{
+		}
+
+		public async Task<IActionResult> OnPost()
+		{
+			if(ModelState.IsValid) {
+				await _db.Book.AddAsync(Book);
+				await _db.SaveChangesAsync();
+				return RedirectToPage("Index");
+			} else {
+				return Page();
+			}
 		}
 	}
 }
